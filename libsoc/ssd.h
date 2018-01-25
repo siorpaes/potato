734154e09337bf907e5129b7e404478549c0fa26
David Siorpaes <david.siorpaes@st.com>
2018-01-25 19:42:03 +0100

    Added very basic software support for SSD peripheral

---
+++ libsoc/ssd.h
@@ -0,0 +1,4 @@
+#define DIGIT0 *((uint32_t*)(PLATFORM_SSD_BASE))
+#define DIGIT1 *((uint32_t*)(PLATFORM_SSD_BASE + 4))
+#define DIGIT2 *((uint32_t*)(PLATFORM_SSD_BASE + 8))
+#define DIGIT3 *((uint32_t*)(PLATFORM_SSD_BASE + 0xc))
--- software/ssd/main.c
+++ software/ssd/main.c
@@ -1,15 +1,13 @@
-/** GPIO sample application for Potato Processor
+/** SSD sample application for Potato Processor
  * david.siorpaes@gmail.com
  */
 
+#include <stdio.h>
 #include <stdint.h>
 
 #include "platform.h"
+#include "ssd.h"
 
-#define DIGIT0 *((uint32_t*)(0xc0006000))
-#define DIGIT1 *((uint32_t*)(0xc0006004))
-#define DIGIT2 *((uint32_t*)(0xc0006008))
-#define DIGIT3 *((uint32_t*)(0xc000600c))
 
 void exception_handler(uint32_t cause, void * epc, void * regbase)
 {
